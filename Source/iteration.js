f923416156b72772b009fc6c38779b136f4a5b8b
Akintunde Oyelakin <113299215+CodewithOye@users.noreply.github.com>
2023-10-19 10:47:44 +0200

    iteration

---
+++ Source/iteration.js
@@ -0,0 +1,25 @@
+/*
+for(i = 0 ; i < 10; i++){
+  console.log(i);
+}*/
+/*
+let a = [4, 8, 15, 16, 23, 42];
+for (i = 0; i < a.length; i++){
+  console.log(a[i])
+}*/
+/*
+let a = [4, 8, 15, 16, 23, 42]
+for (let b = 0; b < a.length; b++) {
+  const c = a[b];
+  console.log(c)
+}
+*/
+
+let x = 1;
+while (x < 10) {
+  console.log(`While statement:  ${x++}`)
+  if(x == 7) break;
+  /*if (x == 7){
+    break;
+  }*/
+}
